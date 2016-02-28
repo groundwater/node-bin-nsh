edda2b2fd3aa2b1a9a11c7f812b83cc9fabb7f3f
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-02-28 14:41:39 +0100

    Tests for 'ignore stdio' and 'pipe two commands between them'

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ var assert      = require('assert')
 var PassThrough = require('stream').PassThrough
 var tty         = require('tty')
 
+var concat = require('concat-stream')
+
 var spawnStream = require('./lib/ast2js/_spawnStream')
 
 
@@ -14,6 +16,46 @@ describe('spawnStream', function()
     assert.strictEqual(result.constructor.name, 'Duplex')
     assert.ok(result.readable)
     assert.ok(result.writable)
+  })
+
+  it('ignore stdio', function()
+  {
+    var stdio = ['ignore', 'ignore']
+
+    var result = spawnStream('ls', {stdio: stdio})
+
+    assert.strictEqual(result.constructor.name, 'EventEmitter')
+    assert.ok(!result.readable)
+    assert.ok(!result.writable)
+  })
+
+  it('pipe two commands between them', function(done)
+  {
+    var expected = ['aa','ab','bb']
+
+    var stdio = [null, concat(function(data)
+    {
+      expected.shift()
+      expected = expected.join('\n')
+
+      assert.strictEqual(data.toString(), expected+'\n')
+
+      done()
+    })]
+
+    var grep = spawnStream('grep', ['b'], {stdio: stdio})
+
+    assert.strictEqual(grep.constructor.name, 'Socket')
+    assert.ok(!grep.readable)
+    assert.ok(grep.writable)
+
+    var argv = ['-e', expected.join('\\n')]
+
+    var echo = spawnStream('echo', argv, {stdio: [null, grep]})
+
+    assert.strictEqual(echo.constructor.name, 'Socket')
+    assert.ok(!echo.readable)
+    assert.ok(echo.writable)
   })
 
   describe('pipe regular streams', function()
@@ -29,11 +71,18 @@ describe('spawnStream', function()
       assert.ok(!result.writable)
     })
 
-    it('stdout', function()
+    it('stdout', function(done)
     {
-      var stdio = [null, new PassThrough()]
+      var expected = 'asdf'
 
-      var result = spawnStream('ls', {stdio: stdio})
+      var stdio = [null, concat(function(data)
+      {
+        assert.strictEqual(data.toString(), expected+'\n')
+
+        done()
+      })]
+
+      var result = spawnStream('echo', [expected], {stdio: stdio})
 
       assert.strictEqual(result.constructor.name, 'Socket')
       assert.ok(!result.readable)
