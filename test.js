5a8d13c141099b7b184994e016e76ef2a7f06ed6
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-02-28 21:37:47 +0100

    Improved tests

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-var assert      = require('assert')
-var PassThrough = require('stream').PassThrough
-var tty         = require('tty')
+var assert = require('assert')
+var tty    = require('tty')
 
 var concat = require('concat-stream')
+var str    = require('string-to-stream')
 
 var spawnStream = require('./lib/ast2js/_spawnStream')
 
@@ -33,7 +33,7 @@ describe('spawnStream', function()
   {
     var expected = ['aa','ab','bb']
 
-    var stdio = [null, concat(function(data)
+    var stdout = concat(function(data)
     {
       expected.shift()
       expected = expected.join('\n')
@@ -41,11 +41,11 @@ describe('spawnStream', function()
       assert.strictEqual(data.toString(), expected+'\n')
 
       done()
-    })]
+    })
 
-    var grep = spawnStream('grep', ['b'], {stdio: stdio})
+    var grep = spawnStream('grep', ['b'], {stdio: [null, stdout]})
 
-    assert.strictEqual(grep.constructor.name, 'Socket')
+    assert.strictEqual(grep.constructor.name, 'Writable')
     assert.ok(!grep.readable)
     assert.ok(grep.writable)
 
@@ -53,91 +53,161 @@ describe('spawnStream', function()
 
     var echo = spawnStream('echo', argv, {stdio: [null, grep]})
 
-    assert.strictEqual(echo.constructor.name, 'Socket')
+    assert.strictEqual(echo.constructor.name, 'Writable')
     assert.ok(!echo.readable)
     assert.ok(echo.writable)
   })
 
   describe('pipe regular streams', function()
   {
-    it('stdin', function()
+    it('stdin', function(done)
     {
-      var stdio = [new PassThrough()]
+      var expected = ['aa','ab','bb']
 
-      var result = spawnStream('ls', {stdio: stdio})
+      var stdin = str(expected.join('\n'))
 
-      assert.strictEqual(result.constructor.name, 'Socket')
-      assert.ok(result.readable)
-      assert.ok(!result.writable)
+      var grep = spawnStream('grep', ['b'], {stdio: [stdin]})
+
+      assert.strictEqual(grep.constructor.name, 'Socket')
+      assert.ok(grep.readable)
+      assert.ok(!grep.writable)
+
+      grep.pipe(concat(function(data)
+      {
+        expected.shift()
+        expected = expected.join('\n')
+
+        assert.strictEqual(data.toString(), expected+'\n')
+      }))
+
+      grep.on('end', done)
     })
 
     it('stdout', function(done)
     {
       var expected = 'asdf'
 
-      var stdio = [null, concat(function(data)
+      var stdout = concat(function(data)
       {
         assert.strictEqual(data.toString(), expected+'\n')
+      })
 
-        done()
-      })]
+      var echo = spawnStream('echo', [expected], {stdio: [null, stdout]})
 
-      var result = spawnStream('echo', [expected], {stdio: stdio})
+      assert.strictEqual(echo.constructor.name, 'Writable')
+      assert.ok(!echo.readable)
+      assert.ok(echo.writable)
 
-      assert.strictEqual(result.constructor.name, 'Socket')
-      assert.ok(!result.readable)
-      assert.ok(result.writable)
+      echo.on('end', done)
     })
 
-    it('fully piped', function()
+    it('fully piped', function(done)
     {
-      var stdio = [new PassThrough(), new PassThrough()]
+      var expected = ['aa','ab','bb']
 
-      var result = spawnStream('ls', {stdio: stdio})
+      var stdin  = str(expected.join('\n'))
+      var stdout = concat(function(data)
+      {
+        expected.shift()
+        expected = expected.join('\n')
 
-      assert.strictEqual(result.constructor.name, 'EventEmitter')
-      assert.ok(!result.readable)
-      assert.ok(!result.writable)
+        assert.strictEqual(data.toString(), expected+'\n')
+      })
+
+      var grep = spawnStream('grep', ['b'], {stdio: [stdin, stdout]})
+
+      assert.strictEqual(grep.constructor.name, 'EventEmitter')
+      assert.ok(!grep.readable)
+      assert.ok(!grep.writable)
+
+      grep.on('end', done)
     })
   })
 
   describe('pipe handler streams', function()
   {
-    it('stdin', function()
+    it('stdin', function(done)
     {
-      var stdio = [new tty.ReadStream()]
+      var stdin = new tty.ReadStream()
 
-      var result = spawnStream('ls', {stdio: stdio})
+      var result = spawnStream('ls', {stdio: [stdin]})
 
       assert.strictEqual(result.constructor.name, 'Socket')
       assert.ok(result.readable)
       assert.ok(!result.writable)
+
+      stdin.on('end', function(){
+        console.log('***end***')
+      })
+      stdin.on('finish', function(){
+        console.log('***finish***')
+      })
+      result.on('end', function()
+      {
+        console.log('***end 2***')
+        stdin.end()
+
+        done()
+      })
     })
 
-    it('stdout', function()
+    it('stdout', function(done)
     {
-      var stdio = [null, new tty.WriteStream()]
+      var stdout = new tty.WriteStream()
 
-      var result = spawnStream('ls', {stdio: stdio})
+      var result = spawnStream('ls', {stdio: [null, stdout]})
 
-      assert.strictEqual(result.constructor.name, 'Socket')
+      assert.strictEqual(result.constructor.name, 'Writable')
       assert.ok(!result.readable)
       assert.ok(result.writable)
+
+      result.on('end', function()
+      {
+
+        done()
+      })
     })
 
-    it('fully piped', function()
+    it('fully piped', function(done)
     {
-      var stdio = [new tty.ReadStream(), new tty.WriteStream()]
+      var stdin  = new tty.ReadStream()
+      var stdout = new tty.WriteStream()
 
-      var result = spawnStream('ls', {stdio: stdio})
+      var result = spawnStream('ls', {stdio: [stdin, stdout]})
 
       assert.strictEqual(result.constructor.name, 'EventEmitter')
       assert.ok(!result.readable)
       assert.ok(!result.writable)
+
+      result.on('end', function()
+      {
+
+        done()
+      })
     })
   })
 })
 
+
+// describe('command', function()
+// {
+//   it('', function(done)
+//   {
+//     var item =
+//     {
+//       command:,
+//       args: [],
+//       stdio:,
+//       redirects: [],
+//       env: {}
+//     }
+//
+//     command(item, function(command)
+//     {
+//
+//     })
+//   })
+// })
 
 // if('inception', function()
 // {
