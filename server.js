e52e60aad814e0bab651902ec9f451f663037ab1
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2017-05-08 14:47:12 +0200

    `eval` function & `-c` argument

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 #!/usr/bin/env node
 
-var Nsh = require('.')
+const Nsh = require('.')
 
 
-Nsh([process.stdin, process.stdout, process.stderr])
+function onerror(error)
+{
+  console.error(process.argv0+': '+error)
+  process.exit(2)
+}
+
+
+const stdio = [process.stdin, process.stdout, process.stderr]
+
+const argv = process.argv.slice(2)
+if(argv.shift() === '-c')
+{
+  if(!argv.length) return onerror('-c requires an argument')
+
+  return Nsh.eval(stdio, argv.join(' '), function(error)
+  {
+    if(error) onerror(error)
+  })
+}
+
+
+Nsh(stdio)
 .on('SIGINT', function()
 {
   this.write('^C')
